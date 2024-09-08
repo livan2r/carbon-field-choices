68fb02877c1f8c6d6ffae4f73754e13975a761e6
Livan Rodriguez <livan2r@gmail.com>
2024-09-08 17:03:50 -0400

    implementing Carbon_Field_Choices

--- core/Choices_Field.php
+++ core/Choices_Field.php
@@ -5,5 +5,24 @@ namespace Carbon_Field_Choices;
 use Carbon_Fields\Field\Select_Field;
 
 class Choices_Field extends Select_Field {
+    /**
+     * Enqueue scripts and styles in admin
+     * Called once per field type
+     */
+    public static function admin_enqueue_scripts() {
+        $root_uri = \Carbon_Fields\Carbon_Fields::directory_to_url( \Carbon_Field_Choices\DIR );
 
+        // Enqueue field styles.
+        wp_enqueue_style(
+            'carbon-field-choice',
+            $root_uri . '/build/bundle' . ( ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min' ) . '.css'
+        );
+
+        // Enqueue field scripts.
+        wp_enqueue_script(
+            'carbon-field-choice',
+            $root_uri . '/build/bundle' . ( ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min' ) . '.js',
+            array( 'carbon-fields-core' )
+        );
+    }
 }
