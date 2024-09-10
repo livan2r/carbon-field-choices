2fdb7c810fc73b0de78682631149b595dc724229
Livan Rodriguez <livan2r@gmail.com>
2024-09-10 14:29:43 -0400

    testSearchEbooks implemented

--- field.php
+++ field.php
@@ -4,6 +4,9 @@ use Carbon_Field_Choices\Choices_Field;
 
 define( 'Carbon_Field_Choices\\DIR', __DIR__ );
 
-Carbon_Fields::extend( Choices_Field::class, function($container ) {
-	return new Choices_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
-} );
+$loaded = defined('ABSPATH');
+if ($loaded) {
+    Carbon_Fields::extend(Choices_Field::class, function ($container) {
+        return new Choices_Field($container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label']);
+    });
+}
